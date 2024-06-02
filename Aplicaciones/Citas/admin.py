2aae7d7d1bfb813489ea34d9f1d2ab57a94833b1
CNUBIY <carlos_mcy_2001@hotmail.com>
2024-06-01 23:32:36 -0500

    Arreglo models.py

--- Aplicaciones/Citas/admin.py
+++ Aplicaciones/Citas/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import DiaHorario, HorasDia, UsuarioCli, GenerosCli
+from .models import DiaHorario, HorasDia, UsuarioCli, GenerosCli, MadreCita, PadreCita, ContactCita, CitaSol
 # Register your models here.
 
 admin.site.register(DiaHorario)
 admin.site.register(HorasDia)
 admin.site.register(UsuarioCli)
 admin.site.register(GenerosCli)
+admin.site.register(MadreCita)
+admin.site.register(PadreCita)
+admin.site.register(ContactCita)
+admin.site.register(CitaSol)
--- Aplicaciones/Citas/models.py
+++ Aplicaciones/Citas/models.py
@@ -10,6 +10,49 @@ class GenerosCli (models.Model):
     def __str__(self):
         return f"- {self.nombre_gen}"
 
+
+class MadreCita(models.Model):
+    id= models.AutoField(primary_key=True)
+    nom_mom=models.CharField(max_length=150)
+    ape_mom=models.CharField(max_length=150)
+    age_mom=models.DateField()
+    hij_mom=models.IntegerField()
+    es_cimom=models.CharField(max_length=150)
+    act_mom=models.CharField(max_length=150)
+    correo_mom=models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"{self.nom_mom} {self.ape_mom}"
+
+class PadreCita(models.Model):
+    id= models.AutoField(primary_key=True)
+    nom_fat=models.CharField(max_length=150)
+    ape_fat=models.CharField(max_length=150)
+    age_fat=models.DateField()
+    act_fat=models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"{self.nom_fat} {self.ape_fat}"
+
+class ContactCita (models.Model):
+    id=models.AutoField(primary_key=True)
+    tf_casa=models.CharField(max_length=150)
+    cell=models.CharField(max_length=150)
+    tf_tra=models.CharField(max_length=150)
+
+
+class CitaSol(models.Model):
+    id=models.AutoField(primary_key=True)
+    fech_da=models.DateField()
+    time_da=models.TimeField()
+
+    def __str__(self):
+        return f"{self.fech_da}-{self.time_da}"
+
+
+
+
+
 class UsuarioCli(models.Model):
     id = models.AutoField(primary_key=True)
     nombre_us = models.CharField(max_length=150)
