27c9481bf9bb20bd9d47883be31a1f3cef6db907
CNUBIY <carlos_mcy_2001@hotmail.com>
2024-06-10 21:47:12 -0500

    Agregado Login Doc

--- Aplicaciones/Historiales/urls.py
+++ Aplicaciones/Historiales/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+    #LOGIN DOCTOR
+    path('logindoc/', views.user_login, name='logindoc'),
+    path('logoutdoc/',views.user_logout, name='logoutdoc'),
     path('doc_inicio/', views.doc_inicio, name='doc_inicio')
 ]
--- Aplicaciones/Historiales/views.py
+++ Aplicaciones/Historiales/views.py
@@ -14,6 +14,31 @@ from Aplicaciones.Citas.middleware import login_required as custom_login_require
 # Create your views here.
 
 
+#Página LOGIN usarios INICIO
+
+def user_login(request):
+    if request.user.is_authenticated:
+        logout(request)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            request.session['last_activity'] = timezone.now().isoformat()  # Convertir a cadena
+            return redirect("doc_inicio")
+        else:
+            messages.error(request, 'Contraseña/Correo Incorrectos')
+    return render(request, 'auth/doc_login.html')
+
+
+def user_logout(request):
+    logout (request)
+    return redirect('/')
+
+
+#Página LOGIN usarios FINAL
 
 @login_required
 def doc_inicio (request):
