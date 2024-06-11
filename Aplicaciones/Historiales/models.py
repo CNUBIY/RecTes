b958e646c2674407c70a0590721dc30b320396b4
CNUBIY <carlos_mcy_2001@hotmail.com>
2024-06-10 22:22:08 -0500

    Añadido Primer modelo paciente

--- Aplicaciones/Historiales/models.py
+++ Aplicaciones/Historiales/models.py
@@ -3,4 +3,11 @@ from django.db import models
 # Create your models here.
 class Patient(models.Model):
     idPat=models.AutoField(primary_key=True)
-    
+    lastnPat=models.CharField(max_length=150)
+    namePat=models.CharField(max_length=150)
+    pldatePat=models.TextField()
+    birthPat=models.DateField()
+    fagePat=models.CharField(max_length=150)
+    natiPat=models.CharField(max_length=150)
+    placePat=models.TextField(max_length=150)
+    ciPat=models.CharField(max_length=10)
