b3cf9ffcc8cd80984a2b1248b52f8ac5cf1b15df
CNUBIY <carlos_mcy_2001@hotmail.com>
2024-06-16 01:32:59 -0500

    Arreglos Validaciones CITAS

--- Aplicaciones/Citas/urls.py
+++ Aplicaciones/Citas/urls.py
@@ -18,11 +18,10 @@ urlpatterns=[
     path('delete_adciIn/<id>/',views.delete_adciIn),
     #CONTABILIDAD
     path('adci_inicio/cont_inicio/', views.cont_inicio),
-    path('adci_inicio/cont_inicio/cont_int/',views.cont_int),
+    path('adci_inicio/cont_inicio/cont_int/',views.cont_int, name='cont_int'),
     path('adci_inicio/addcont_adci/<id>',views.addcont_adci, name='addcont_adci'),
     path('adci_inicio/aggcont_adci/', views.aggcont_adci, name='aggcont_adci'),
-    path('adci_inicio/cont_vac/',views.cont_vac, name='cont_vac'),
-    path('adci_inicio/cont_vac/aggcont_vac/', views.aggcont_vac, name="aggcont_vac"),
+
     #HORARIOS ADMIN
     path('adci_fechacitas/',views.adci_fechacitas),
     path('aggagenda_adci/',views.aggagenda_adci),
--- Aplicaciones/Citas/views.py
+++ Aplicaciones/Citas/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from Aplicaciones.Citas.middleware import login_required as custom_login_required
+from collections import defaultdict, OrderedDict
 # Create your views here.
 
 #Página Informativa INICIO
@@ -98,6 +99,9 @@ def adci_inicio(request):
     else:
         mañana = hoy + timedelta(days=1)
 
+    # Eliminar las citas no confirmadas y no cortas para fechas pasadas
+    CitaSol.objects.filter(est_da=False, cort_da=False, fech_da__lt=hoy).delete()
+
     # Ordenar las citas por el campo 'time_da'
     citas_hoy = CitaSol.objects.filter(fech_da=hoy, cort_da=False).order_by('time_da')
     citas_mañana = CitaSol.objects.filter(fech_da=mañana).order_by('time_da')
@@ -110,6 +114,7 @@ def adci_inicio(request):
     }
 
     return render(request, 'adci_inicio.html', context)
+
 
 @login_required
 @custom_login_required
@@ -132,7 +137,7 @@ def aggin_adci(request):
 
 @login_required
 @custom_login_required
-def procesarActualizacionHorarioIn(request,id):
+def procesarActualizacionHorarioIn(request, id):
     if request.method == 'POST':
         try:
             nom_da = request.POST["nom_da"]
@@ -141,8 +146,25 @@ def procesarActualizacionHorarioIn(request,id):
             time_da = request.POST["time_da"]
             cort_da = request.POST.get("cort_da") == "on"
 
+            #horarios de servicio
+            selected_datetime = datetime.strptime(f"{fech_da} {time_da}", "%Y-%m-%d %H:%M")
+            selected_time = selected_datetime.time()
+            if not ((8 <= selected_time.hour < 13) or (16 <= selected_time.hour < 20)):
+                messages.error(request, "No puede ingresar horarios fuera de servicio.")
+                return redirect('/adci_inicio')
 
-            # Procesar la actualización de la cita con los valores obtenidos
+            # horas pasadas
+            current_datetime = datetime.now()
+            if fech_da == current_datetime.strftime("%Y-%m-%d") and selected_datetime < current_datetime - timedelta(hours=1):
+                messages.error(request, "No puedes seleccionar una hora pasada para la fecha de hoy.")
+                return redirect('/adci_inicio')
+
+            # fechas registradas
+            citas_existentes = CitaSol.objects.filter(fech_da=fech_da, time_da=time_da).exclude(pk=id)
+            if citas_existentes.exists():
+                messages.error(request, "Ya existe una cita registrada para esta fecha y hora.")
+                return redirect('/adci_inicio')
+
             cita = get_object_or_404(CitaSol, pk=id)
             cita.nom_da = nom_da
             cita.telf_da = telf_da
@@ -151,10 +173,15 @@ def procesarActualizacionHorarioIn(request,id):
             cita.cort_da = cort_da
             cita.save()
 
+            messages.success(request, "Cita actualizada correctamente.")
             return redirect('/adci_inicio')
         except MultiValueDictKeyError as e:
             # Manejo de error cuando no se encuentra un campo esperado en request.POST
             return HttpResponseBadRequest(f"Missing parameter: {e}")
+        except Exception as e:
+            print(f"Error al procesar la solicitud: {str(e)}")
+            messages.error(request, "Ha ocurrido un error al procesar la solicitud.")
+            return redirect('/error_p')
     else:
         return redirect('/error_p')
 
@@ -181,21 +208,52 @@ def adci_fechacitas (request):
 @login_required
 @custom_login_required
 def aggagenda_adci(request):
-    nom_da=request.POST["nom_da"]
-    telf_da=request.POST["telf_da"]
-    fech_da=request.POST["fech_da"]
-    time_da=request.POST["time_da"]
-    cort_da = request.POST.get("cort_da") == "on"
+    if request.method == 'POST':
+        try:
+            nom_da = request.POST["nom_da"]
+            telf_da = request.POST["telf_da"]
+            fech_da = request.POST["fech_da"]
+            time_da = request.POST["time_da"]
+            cort_da = request.POST.get("cort_da") == "on"
 
-    nuevoHorarioDia = CitaSol.objects.create(
-    fech_da=fech_da,
-    telf_da=telf_da,
-    nom_da=nom_da,
-    time_da=time_da,
-    cort_da=cort_da,
-    est_da=False,
-    )
-    return redirect('/adci_inicio')
+            # Validar que el horario esté dentro de los rangos permitidos (8am - 8pm)
+            selected_time = datetime.strptime(time_da, "%H:%M").time()
+            if not ((8 <= selected_time.hour < 13) or (16 <= selected_time.hour < 20)):
+                messages.error(request, "No puede ingresar horarios fuera de servicio.")
+                return redirect('/adci_fechacitas')
+
+            # Validar que no se pueda seleccionar una hora pasada para la fecha actual
+            current_datetime = datetime.now()
+            selected_datetime = datetime.strptime(f"{fech_da} {time_da}", "%Y-%m-%d %H:%M")
+            if fech_da == current_datetime.strftime("%Y-%m-%d") and selected_datetime < current_datetime - timedelta(hours=1):
+                messages.error(request, "No puedes seleccionar una hora pasada para la fecha de hoy.")
+                return redirect('/adci_fechacitas')
+
+            # Verificar si la fecha y hora ya están registradas en otra cita
+            citas_existentes = CitaSol.objects.filter(fech_da=fech_da, time_da=time_da)
+            if citas_existentes.exists():
+                messages.error(request, "Ya existe una cita registrada para esta fecha y hora.")
+                return redirect('/adci_fechacitas')
+
+            # Procesar la creación de la cita si pasa todas las validaciones
+            nueva_cita = CitaSol.objects.create(
+                fech_da=fech_da,
+                telf_da=telf_da,
+                nom_da=nom_da,
+                time_da=time_da,
+                cort_da=cort_da,
+                est_da=False,
+            )
+            messages.success(request, "Cita registrada correctamente.")
+            return redirect('/adci_inicio')
+
+        except Exception as e:
+            print(f"Error al procesar la solicitud: {str(e)}")
+            messages.error(request, "Ha ocurrido un error al procesar la solicitud.")
+            return redirect('/error_p')
+    else:
+    # Manejar solicitudes GET o cualquier otro método HTTP no permitido
+        return redirect('/error_p')
 
 @login_required
 @custom_login_required
@@ -286,9 +344,11 @@ def addcont_adci(request, id):
 @login_required
 @custom_login_required
 def cont_int(request):
-    citabdd = CitaSol.objects.all()  # Obtener todas las citas
+    # Obtener todas las citas y facturas
+    citabdd = CitaSol.objects.all()
     factbdd = FactCitas.objects.all()
 
+    # Crear un diccionario para almacenar las citas por fecha
     citas_por_fecha = defaultdict(lambda: {
         'matutina': {'atendidos': 0, 'ventas': 0, 'cortesias': 0},
         'vespertina': {'atendidos': 0, 'ventas': 0, 'cortesias': 0}
@@ -301,13 +361,27 @@ def cont_int(request):
         if cita.est_da:
             citas_por_fecha[cita.fech_da][turno]['atendidos'] += 1
 
+    total_anual = 0
     for fact in factbdd:
-        if fact.fechfac.est_da:  # Solo considerar las citas con est_da=True
+        if fact.fechfac and fact.fechfac.est_da:
             turno = 'matutina' if fact.fechfac.time_da < time(12) else 'vespertina'
             citas_por_fecha[fact.fechfac.fech_da][turno]['ventas'] += fact.valfac
+            if fact.fechfac.fech_da.year == datetime.now().year:  # Verificar si la fecha es del año actual
+                total_anual += fact.valfac
+
+    # Ordenar citas_por_fecha por fech_da de más actual a más antigua
+    citas_por_fecha_ordenadas = OrderedDict(
+        sorted(citas_por_fecha.items(), key=lambda item: item[0], reverse=True)
+    )
+
+    # Crear un diccionario para el total anual
+    totales = {
+        'total_anual': total_anual
+    }
 
     context = {
-        'citas_por_fecha': dict(citas_por_fecha)  # Convertir defaultdict a dict
+        'citas_por_fecha': citas_por_fecha_ordenadas,
+        'totales': totales  # Agregar el total anual al contexto
     }
 
     return render(request, 'cont_int.html', context)
