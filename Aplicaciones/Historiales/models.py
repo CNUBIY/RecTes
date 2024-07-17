c574e1343ce4ca26e97edaf6a1d192a580d222ec
CNUBIY <carlos_mcy_2001@hotmail.com>
2024-07-17 03:43:33 -0500

    Agregado tabla alergias

--- Aplicaciones/Historiales/models.py
+++ Aplicaciones/Historiales/models.py
@@ -62,3 +62,11 @@ class Patient(models.Model):
 
     def __str__(self):
         return f"{self.lastnPat} {self.namePat} - {self.ciPat}"
+
+
+class Alergia(models.Model):
+    id=models.AutoField(primary_key=True)
+    creation=models.DateField(default=datetime.date.today)
+    nombreAlergia=models.CharField(max_length=150)
+    def __str__(self):
+        return f"{self.id}. {self.nombreAlergia}"
