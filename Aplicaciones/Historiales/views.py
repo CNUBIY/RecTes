7918877fe8d291801fa0a10e890ba8e71240cd50
CNUBIY <carlos_mcy_2001@hotmail.com>
2024-07-29 19:46:36 -0500

    Ajustes rendimiento gráficos oms 8/14

--- Aplicaciones/Historiales/views.py
+++ Aplicaciones/Historiales/views.py
@@ -689,7 +689,6 @@ def generate_height_chart_girls(request, idPat):
         print(f"Error en generate_height_chart_girls: {e}")
         return None
 
-
 @login_required
 def generate_head_circumference_chart_girls(request, idPat):
     try:
@@ -786,7 +785,6 @@ def generate_head_circumference_chart_girls(request, idPat):
         print(f"Error en generate_head_circumference_chart_girls: {e}")
         return None
 
-
 @login_required
 def generate_bmi_chart_girls(request, idPat):
     try:
@@ -867,15 +865,26 @@ def generate_bmi_chart_girls(request, idPat):
         # Agregar leyenda
         ax.legend()
 
-        # Guardar gráfico en memoria
-        buffer = BytesIO()
-        plt.savefig(buffer, format='png')
-        buffer.seek(0)
-        image_png = buffer.getvalue()
-        buffer.close()
-        bmi_chart_girls = base64.b64encode(image_png).decode('utf-8')
+        # Guardar gráfico en el directorio de medios
+        media_dir = settings.MEDIA_ROOT
+        bmi_charts_dir = os.path.join(media_dir, 'bmi_charts_girls')
+        if not os.path.exists(bmi_charts_dir):
+            os.makedirs(bmi_charts_dir)
 
-        return bmi_chart_girls
+        # Eliminar la imagen anterior si existe
+        image_path = os.path.join(bmi_charts_dir, f'bmi_chart_girls_{idPat}.png')
+        if os.path.exists(image_path):
+            os.remove(image_path)
+
+        # Guardar la nueva imagen
+        plt.savefig(image_path, format='png')
+        plt.close(fig)  # Cerrar el gráfico para liberar memoria
+
+        # Codificar la imagen en base64 para devolverla como una cadena
+        with open(image_path, "rb") as image_file:
+            graphic = base64.b64encode(image_file.read()).decode('utf-8')
+
+        return graphic
 
     except Exception as e:
         print(f"Error en generate_bmi_chart_girls: {e}")
