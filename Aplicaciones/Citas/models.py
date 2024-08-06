e0d99261e0d006720733bf7214b88554057ea648
CNUBIY <carlos_mcy_2001@hotmail.com>
2024-08-06 00:23:35 -0500

    Ajustes modelo Modulo citas

--- Aplicaciones/Citas/models.py
+++ Aplicaciones/Citas/models.py
@@ -16,7 +16,8 @@ class CitaSol(models.Model):
     correo_da=models.CharField(null=True, blank=True, max_length=250)
     est_da=models.BooleanField(default=False)
     notificacion_enviada = models.BooleanField(default=False)
-
+    cancelado=models.BooleanField(default=False)
+    comentario = models.TextField(null=True,blank=True)
     def __str__(self):
         time_formatted = self.time_da.strftime("%I:%M %p")  # Formato de 12 horas con AM/PM sin segundos
         return f"{self.nom_da}: {self.fech_da} a las {time_formatted} por favor comunicarse a {self.telf_da}"
@@ -29,7 +30,7 @@ class FactCitas(models.Model):
     valfac=models.DecimalField(max_digits=5, decimal_places=2)
     obsfac=models.CharField(max_length=150)
     fechfac=models.ForeignKey(CitaSol,null=True, blank=True, on_delete=models.PROTECT)
-
+    cancelado=models.BooleanField(default=False)
     def __str__(self):
         return f"{self.idfac}-{self.fechfac}"
 
